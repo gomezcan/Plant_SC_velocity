40fecf7263af35ac9675d455a082cd453c1d39d1
gomezcan <46493296+gomezcan@users.noreply.github.com>
2022-05-23 12:37:08 -0400

    Create CellRanger_counts.single.sample.sh
    example for a single sample

---
+++ src/CellRanger_counts.single.sample.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+########## BATCH Lines for Resource Request ##########
+#SBATCH --time=1:00:00          	# Limit of wall clock time - how long the job will run (same as -t)
+#SBATCH --nodes=1            		  # Number of different nodes - could be an exact number or a range of nodes (same as -N)
+#SBATCH --ntasks=1          		  # Number of tasks - how many tasks (nodes) that you require (same as -n)
+#SBATCH --cpus-per-task=20     		# Number of CPUs (or cores) per task (same as -c)
+#SBATCH --mem=100G       	       	# Specify the real memory required per node. (20G)
+#SBATCH --job-name CR_count 	    # You can give your job a name for easier identification (same as -J)
+
+
+########## Load Modules #########
+
+
+module purge
+
+File=$/mnt/home/gomezcan/Projects/SingleCell/RawData/
+
+cellranger count --id=$1 \
+	--transcriptome=/mnt/home/gomezcan/Projects/SingleCell/Genomes/Index_TAIR11_CellRange/ \
+	--fastqs=$File/$1 \
+	--sample=Sample_$1 \
+	--localcores=20 --localmem=100
