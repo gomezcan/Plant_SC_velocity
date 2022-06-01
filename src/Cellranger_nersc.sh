ab889a3c0334b195fa7d4b69249dad0cfb00a0b3
gomezcan <46493296+gomezcan@users.noreply.github.com>
2022-06-01 09:24:16 -0400

    Update Cellranger_nersc.sh
    update

--- src/Cellranger_nersc.sh
+++ src/Cellranger_nersc.sh
@@ -1,1 +1,39 @@
+#!/bin/bash
+########## BATCH Lines for Resource Request ##########
+#SBATCH --time=110:00:00          	# Limit of wall clock time - how long the job will run (same as -t)
+#SBATCH --nodes=1            		# Number of different nodes - could be an exact number or a range of nodes (same as -N)
+#SBATCH --ntasks=1          		# Number of tasks - how many tasks (nodes) that you require (same as -n)
+#SBATCH --cpus-per-task=100    		# Number of CPUs (or cores) per task (same as -c)
+#SBATCH --mem=60G       	       	# Specify the real memory required per node. (20G)
+#SBATCH --job-name CRcounts 	     	# You can give your job a name for easier identification (same as -J)
+#SBATCH -A egl 
 
+
+#########################################
+########## 	Load Modules 	#########
+#########################################
+module purge
+#########################################
+
+
+# Genome index
+Index_TAIR11_CellRange=/mnt/home/gomezcan/Projects/SingleCell/Genomes/Index_TAIR11_CellRange
+
+
+for f in SRR1*; do
+
+	# Sample to run
+	Sample=${f}
+
+	# require: fastq files on sample_diratory ($Sample)
+	Sample_dir=$Sample
+	
+	cellranger count --id=Counts_$Sample \
+		--transcriptome=$Index_TAIR11_CellRange \
+		--fastqs=$Sample_dir \
+		--sample=$Sample \
+        	--localcores=100 \
+		--localmem=100 \
+		--include-introns true
+
+done;
